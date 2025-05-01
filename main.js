1cb2bd2f4d91ca1f0b8a0552e940a1f30fe3afae
vermagithubcs <vermadigital943@gmail.com>
2025-05-01 15:24:56 +0530

    update the files again

--- main.js
+++ main.js
@@ -7,9 +7,6 @@ app.get('/',function(req,res){
 app.get('/about',function(req,res){
     res.send(`<h1>Please Login first then visit about page.</h1>`)
 })
-app.get('/chai',function(req,res){
-    res.send('Chai pasand ha merko pina ha abhi..')
-})
 app.listen(process.env.PORT,()=>{
     console.log(`Port listening ${process.env.PORT}`);
 });
